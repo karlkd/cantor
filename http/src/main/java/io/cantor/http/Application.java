18d4056c8ef7c41e861544990eb7015b725aa19b
liuchang <chang.liu@56qq.com>
2018-09-08 14:04:47 +0800

    feat: implements http server

---
+++ http/src/main/java/io/cantor/http/Application.java
@@ -0,0 +1,11 @@
+package io.cantor.http;
+
+
+@FunctionalInterface
+public interface Application {
+
+    void handle(AffinityScheduler scheduler,
+                HandlerRequest handlerRequest,
+                HandlerResponse handlerResponse) throws Exception;
+
+}
